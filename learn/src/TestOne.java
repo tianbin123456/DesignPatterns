b53cad1516b3f41ab6bac914b4d44f7b11e04d37
tianbin9611 <tianbin9611@zto.com>
2020-11-25 16:25:59 +0800

    初始化

---
+++ learn/src/TestOne.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: tianbin9611
+ * @Date: 2020/11/25 16:24
+ * @Description:
+ */
+public class TestOne {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
